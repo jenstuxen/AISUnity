2c4b29bdae92a5fa28cd25375a82ed2b2486a2ce
Jens Tuxen <mail@jenstuxen.com>
2014-09-12 17:43:09 +0200

    script for using AisView rest api

---
+++ Assets/AisViewClient.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Net.HttpWebRequest;
+/*
+ * Implements the REST services of AisView (http://github.com/dmadk/AisView) 
+ * 
+ */
+public class AisViewClient : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+
+		TextAsset ping() 
+		{
+			System.Net.HttpWebRequest wb = new System.Net.HttpWebRequest ();
+		}
+}
+
