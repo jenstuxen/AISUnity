ca1b6070c4db8cbc2e815701acabbd108b309b66
Jens Tuxen <mail@jenstuxen.com>
2014-09-15 13:40:10 +0200

    AISVIEW rest client

--- Assets/AisViewClient.cs
+++ Assets/AisViewClient.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Net;
 using System;
+using System.IO;
+using SimpleJSON;
 /*
  * Implements the REST services of AisView (http://github.com/dmadk/AisView) 
  * 
  */
-public class AisViewClient : MonoBehaviour
+public class AisViewClient
 {
 		protected Uri baseUri = new Uri(Environment.GetEnvironmentVariable("AISVIEW_URI"));
 		protected String username = Environment.GetEnvironmentVariable("AISVIEW_USERNAME");
@@ -16,12 +18,9 @@ public class AisViewClient : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-			Uri uri = new Uri (baseUri, "/aisview/rest/vessel_list");
-
-			WebRequest wb = WebRequest.Create(uri);
-			authenticate (wb);	
-			Debug.Log (wb.GetResponse ().ToString ());
-
+			if (this.ping ()) {
+			Debug.Log ("pong!");
+			}
 		}
 	
 		// Update is called once per frame
@@ -30,17 +29,64 @@ public class AisViewClient : MonoBehaviour
 	
 		}
 
+		public JSONNode vessel_list(float topLat, float topLon, float botLat, float botLon) {
+		String topLatS = "topLat="+topLat.ToString ("R"); 
+		String topLonS = "topLon="+topLon.ToString ("R");
+		String botLatS = "botLat="+botLat.ToString ("R");
+		String botLonS = "botLon="+botLon.ToString ("R");
 
-		TextAsset ping() 
-		{
-			return null;
-			//System.Net.HttpWebRequest wb = new System.Net.HttpWebRequest ();
+		String param = topLatS + "&" + topLon + "&" + botLat + "&" + botLon;
+
+		return requestJSON ("/vessel_list?"+param);
 		}
 
-		void authenticate(WebRequest wb) 
+		public JSONNode vessel_list() {
+		return requestJSON ("/vessel_list");
+		}
+
+		public Boolean ping() 
+		{
+			return requestString("/ping").Contains("pong");
+		}
+
+		public string requestString (WebRequest wb)
+		{
+			WebResponse webResponse = wb.GetResponse ();
+			StreamReader reader = new StreamReader (webResponse.GetResponseStream ());
+			string response = reader.ReadToEnd ();
+			reader.Close ();
+			webResponse.Close ();
+			return response;
+		}
+
+		public string requestString (String name)
+		{
+			Uri uri = new Uri (baseUri, name);
+			WebRequest wb = request (uri);
+			return requestString (wb);
+		}
+
+		public JSONNode requestJSON(String name)
+		{
+			string data = requestString (name);
+			JSONNode jsonData = JSON.Parse(data);
+			return jsonData;
+		}
+		
+
+		private void authenticate(WebRequest wb) 
 		{
 			String encoded = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(username + ":" + password));
 			wb.Headers.Add("Authorization", "Basic " + encoded);
+
+		}
+
+		private WebRequest request(Uri uri)
+		{
+			ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+			WebRequest wb = WebRequest.Create(uri);
+			authenticate (wb);
+			return wb;
 
 		}
 }
